a074267d55afd18b2260f9c8265c30e7e5230131
Lollipop <291950304@qq.com>
2022-06-29 23:23:14 +0800

    chore: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   meta: {
-    title: 'Fan\' Bolg'
+    title: 'Fan\' Bolg',
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/images/favicon.ico' },
+    ],
   },
   modules: [
     '@unocss/nuxt',
