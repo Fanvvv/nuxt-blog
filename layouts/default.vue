9548d0a5c4af678b1d210d153126593a5d82072d
Lollipop <291950304@qq.com>
2022-06-29 23:28:50 +0800

    chore: footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
-  <div color-cyan>
-    hello world!
+  <div mx-a max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0>
+    <TheHeader />
+    <main>
+      <slot />
+    </main>
+    <TheFooter />
   </div>
-  <!-- <TheHeader /> -->
 </template>
