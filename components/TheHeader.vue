1307ff6d415c58390f227c3eb6bae62000ccaca3
Lollipop <291950304@qq.com>
2022-06-30 22:18:29 +0800

    feat: dark

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,28 +1,37 @@
 <template>
   <div f-cb>
     <header fcb py-10>
-      <div fcb>
-        <img h15 mr-1 src="/images/logo.png" alt="">
-        <span text-2xl font-serif>' Blog</span>
-      </div>
-      <div fc text-base leading-5>
+      <NuxtLink fcb to="/">
+        <img h15 mr-1 :src="`/images/logo${isDark ? '-dark' : ''}.png`" alt="">
+        <span text-2xl font-serif dark:text-gray-100 hidden md:inline-block>' Blog</span>
+      </NuxtLink>
+      <div fc text-base leading-5 dark:text-gray-100>
         <NuxtLink
           v-for="route in routes"
           :key="route.path"
           :to="route.path"
+          :title="route.name"
           ml-5
         >
-          <span text-gray-900>{{ route.name }}</span>
+          <span text-5 icon-btn :class="route.icon"></span>
         </NuxtLink>
+        <div ml-5 @click="toggleDark" :title="`${isDark ? 'Light' : 'Dark'}`">
+          <span text-5 icon-btn class="dark:i-bi-moon-stars i-bi-brightness-high"></span>
+        </div>
       </div>
     </header>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { isDark } from '~~/composables/dark';
 const routes = reactive([
-  { name: "文章", path: "/posts" },
-  { name: "标签", path: "/tags" },
-  { name: "关于", path: "/about" }
+  { name: "文章", icon: "i-bi-book", path: "/posts"},
+  { name: "标签", icon: "i-bi-tags", path: "/tags" },
+  { name: "关于", icon: "i-bi-emoji-kiss", path: "/about" }
 ])
+
+const toggleDark = () => {
+  isDark.value = !isDark.value
+}
 </script>
