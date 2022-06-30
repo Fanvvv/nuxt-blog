9bbbca64d237cb507f679fdc368051d7cf23e26e
Lollipop <291950304@qq.com>
2022-07-01 01:09:45 +0800

    fix: scroll bug

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div relative z-5 fcc h="[calc(100vh-150px)]">
+  <div relative z-5 fcc h="[calc(100vh-140px)]" ref="heightRef">
     <div px-4 text-3xl md:text-4xl ref="divRef" dark:text-gray-100>
       <span op-0 transition-opacity v-for="(item, index) in title" :key="index" :style="`transition-delay: ${0.2 * (index + 1)}s`">{{ item }}</span>
     </div>
@@ -15,11 +15,11 @@ onMounted(() => {
   divRef.value?.classList.add('show')
 })
 
+const heightRef = ref(null)
 const handleScorll = () => {
-  const clock = setInterval(() => {
-    document.documentElement.scrollTop <= window.innerHeight ? document.documentElement.scrollTop += 10 : clearInterval(clock)
-    document.documentElement.scrollTop + document.documentElement.clientHeight === document.documentElement.scrollHeight && clearInterval(clock)
-  }, 1)
+  const height = heightRef.value?.offsetHeight
+  // 盒子高度 + 100vh所减去的高度
+  document.documentElement.scrollTop = height + 140
 }
 
 const iconArr = ['i-twemoji-carousel-horse', 'i-twemoji-carp-streamer', 'i-twemoji-bullseye', 'i-twemoji-clown-face', 'i-twemoji-confetti-ball', 'i-twemoji-cloud-with-snow']
