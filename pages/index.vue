b1eb33f7b5d6ec0147598a4bbe5da484483c42f2
Lollipop <291950304@qq.com>
2022-06-30 22:16:27 +0800

    chore: scroll icon

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div relative z-5 fcc h="[calc(100vh-150px)]">
-    <div px-4 text-3xl md:text-4xl ref="divRef">
+    <div px-4 text-3xl md:text-4xl ref="divRef" dark:text-gray-100>
       <span op-0 transition-opacity v-for="(item, index) in title" :key="index" :style="`transition-delay: ${0.2 * (index + 1)}s`">{{ item }}</span>
     </div>
-    <div nextAnimate @click="handleScorll">👇</div>
+    <div nextAnimate @click="handleScorll"><span icon-btn text-10 :class="randomIcon()"></span></div>
   </div>
 </template>
 
@@ -21,10 +21,15 @@ const handleScorll = () => {
     document.documentElement.scrollTop + document.documentElement.clientHeight === document.documentElement.scrollHeight && clearInterval(clock)
   }, 1)
 }
+
+const iconArr = ['i-twemoji-carousel-horse', 'i-twemoji-carp-streamer', 'i-twemoji-bullseye', 'i-twemoji-clown-face', 'i-twemoji-confetti-ball', 'i-twemoji-cloud-with-snow']
+const randomIcon = () => {
+  return iconArr[Math.floor(Math.random() * iconArr.length)]
+}
 </script>
 
 <style scoped>
-.show > span{
+.show > span {
   opacity: 1;
 }
 </style>
