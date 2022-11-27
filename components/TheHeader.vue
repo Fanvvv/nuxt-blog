c00ff3b5eb813460febd9760728c274e884fbe50
fan <291950304@qq.com>
2022-11-27 19:52:36 +0800

    chore: 完善header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,7 +2,7 @@
   <div f-cb>
     <header fcb py-10>
       <NuxtLink fcb to="/">
-        <img h15 mr-1 :src="`/images/logo${isDark ? '-dark' : ''}.png`" alt="">
+        <img h15 mr-1 :src="`/images/logo${theme === 'dark' ? '-dark' : ''}.png`" alt="">
         <span text-2xl font-serif dark:text-gray-100 hidden md:inline-block>' Blog</span>
       </NuxtLink>
       <div fc text-base leading-5 dark:text-gray-100>
@@ -15,7 +15,7 @@
         >
           <span text-5 icon-btn :class="route.icon"></span>
         </NuxtLink>
-        <div ml-5 @click="toggleDark" :title="`${isDark ? 'Light' : 'Dark'}`">
+        <div ml-5 @click="toggleDark()" :title="`${isDark ? 'Light' : 'Dark'}`">
           <span text-5 icon-btn class="dark:i-bi-moon-stars i-bi-brightness-high"></span>
         </div>
       </div>
@@ -24,14 +24,15 @@
 </template>
 
 <script lang="ts" setup>
-import { isDark } from '~~/composables/dark';
+import useTheme from '~/composables/useTheme'
 const routes = reactive([
   { name: "文章", icon: "i-bi-book", path: "/posts"},
   { name: "标签", icon: "i-bi-tags", path: "/tags" },
   { name: "关于", icon: "i-bi-emoji-kiss", path: "/about" }
 ])
 
-const toggleDark = () => {
-  isDark.value = !isDark.value
-}
+// 监听 useDark 的变化
+const { isDark, theme } = useTheme()
+
+const toggleDark =  useToggle(isDark)
 </script>
