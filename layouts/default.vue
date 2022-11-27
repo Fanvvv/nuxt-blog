a11ef34d343168ce1826b113162d9f78dc117511
fan <291950304@qq.com>
2022-11-27 19:53:36 +0800

    chore: change bg

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,20 @@
 <template>
-  <div mx-a max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0>
+  <div mx-a max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0 min-w-sm>
     <TheHeader />
     <main>
       <slot />
     </main>
     <TheFooter />
+    <!-- <Plum /> -->
+    <Sun v-show="theme !== 'dark' && width > 1000" />
+    <StarSky v-show="theme === 'dark'" />
   </div>
-  <Plum />
 </template>
+
+<script setup lang="ts">
+import useTheme from '~/composables/useTheme'
+const { theme } = useTheme()
+
+const { width } = useWindowSize()
+
+</script>
