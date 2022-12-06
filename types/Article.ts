685986589cbda965dbc2f19bc5406c95700a1653
fan <291950304@qq.com>
2022-12-06 22:16:26 +0800

    chore: type Article

---
+++ types/Article.ts
@@ -0,0 +1,21 @@
+export type Article = {
+  _path: string,
+  _dir: string,
+  _draft: boolean,
+  _partial: boolean,
+  _locale: string,
+  _empty: boolean,
+  _type: string,
+  _id: string,
+  _source: string,
+  _file: string,
+  _extension: string,
+
+  title: string,
+  description: string,
+  excerpt: object,
+  tags: string[],
+  uid?: number,
+  createtime: string,
+  body: object,
+}
