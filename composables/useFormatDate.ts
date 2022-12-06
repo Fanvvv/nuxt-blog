047a0daf2bf49b968888d9a12a07459db6b8a108
fan <291950304@qq.com>
2022-12-06 22:13:36 +0800

    chore: format date

---
+++ composables/useFormatDate.ts
@@ -0,0 +1,11 @@
+const useFormatDate = (date: string) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  const now = new Date(date).toLocaleDateString('zh-CN', options)
+  return now
+}
+
+export default useFormatDate
