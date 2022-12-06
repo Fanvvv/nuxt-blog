35700cf639c22f5caede32b3295ed0a36997c9ed
fan <291950304@qq.com>
2022-12-06 22:05:52 +0800

    chore: change theme breakpoints

--- unocss.config.ts
+++ unocss.config.ts
@@ -47,4 +47,13 @@ export default defineConfig({
     transformerVariantGroup(),
   ],
   safelist: 'prose prose-sm m-auto text-left'.split(' '),
+  theme: {
+    breakpoints: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'xl2': '1600px',
+    }
+  }
 })
