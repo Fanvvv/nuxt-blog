9c1f9108666c8b521637461c84473b389d25d24f
fan <291950304@qq.com>
2022-12-06 22:06:30 +0800

    chore: Moon Component

---
+++ components/Moon.vue
@@ -0,0 +1,13 @@
+<template>
+  <div fixed top-20 bottom-0 right-30 pointer-events-none class="-z-2 moon">
+
+  </div>
+</template>
+
+<style scoped>
+.moon {
+  background: url('/images/moon.svg') no-repeat;
+  width: 10rem;
+  height: 10rem;
+}
+</style>
