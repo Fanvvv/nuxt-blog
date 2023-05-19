0af8f6c4df763f952fde94e99067ebc7f75c882d
fan <291950304@qq.com>
2023-05-19 23:34:18 +0800

    chore: change footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,14 +4,14 @@
       <div mb-2 text-sm text-gray-500>
         [遇到有趣的事 <span icon-btn color-red hover-color-red-600 animate-bounce animate-count-infinite animate-duration-1s class="i-bi-heart-pulse-fill"></span> 遇到有趣的人]
       </div>
-      <div mb-2 text-sm text-gray-500>
+      <div mb-5 text-sm text-gray-500>
         Copyright © 2022 Fan
       </div>
-      <div mb-8 text-sm text-gray-500>
+      <!-- <div mb-8 text-sm text-gray-500>
         <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">
           湘ICP备2021009899号-1
         </a>
-      </div>
+      </div> -->
     </div>
   </footer>
 </template>
