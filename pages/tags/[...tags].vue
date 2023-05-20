3985c99dd55bb6c311b5b3e920f2fdc834c9551e
fan <291950304@qq.com>
2023-05-20 16:14:50 +0800

    chore: add tags list page

---
+++ pages/tags/[...tags].vue
@@ -0,0 +1,87 @@
+<style scoped>
+.tags-container {
+  @apply font-mono dark:text-gray-300 my-0 mx-auto v-motion-slide-visible-bottom;
+}
+.input-style {
+  @apply block box-border w-full h-12 rounded-2xl border border-gray-300 px-4 py-2 text-gray focus:border-gray focus:ring-gray-500 dark:bg-transparent;
+}
+.search-icon {
+  @apply absolute right-5 top-3 h-6 w-6 text-gray-400 dark:text-gray;
+}
+</style>
+<template>
+  <div article class="tags-container">
+    <h1 text-center capitalize>{{ contain }}</h1>
+    <div relative mt-5>
+      <input
+        aria-label="搜索 blog 里的文章"
+        type="text"
+        placeholder="搜索 blog 里的文章"
+        @input="(e) => searchValue = (e.target as HTMLInputElement).value"
+        class="input-style"
+      />
+      <svg
+        class="search-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </div>
+    <ContentList 
+      path="/posts"
+      :query="{
+        only: ['title', 'description', 'tags', 'date', '_path'],
+        where: {
+          tags: {
+            $contains: contain,
+          },
+        },
+        $sensitivity: 'base',
+      }"
+    >
+      <template #default="{ list }">
+        <div v-if="!filteredBlogPosts(list)?.length">
+          暂无数据
+        </div>
+        <div v-else>
+          <ArticleCard v-if="filteredBlogPosts(list)?.length" :list="sortArticles(filteredBlogPosts(list))" />
+          <ArticleCard v-else :list="sortArticles(list)" />
+        </div>
+      </template>
+    </ContentList>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { Article } from '~/types/Article'
+import ArticleCard from '~/components/ArticleCard.vue'
+
+// 通过时间排序
+function sortArticles(list: Article[]) {
+  return list.sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime()
+  })
+}
+
+const searchValue = ref('')
+
+// 通过标题、描述、标签等搜索过滤博客文章
+const filteredBlogPosts = (list: Article[]) => {
+  return list.filter((frontMatter) => {
+    const searchContent = frontMatter.title + frontMatter.description + frontMatter.tags.join(' ')
+    return searchContent.toLowerCase().includes(searchValue.value.toLowerCase())
+  })
+}
+
+const { params } = useRoute()
+// 从路由中取标签
+const contain = (params.tags as string[]).join('')
+</script>
