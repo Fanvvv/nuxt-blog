b16ae508a2906ff0b453719e6b8626d349b4114d
fan <291950304@qq.com>
2023-05-20 16:17:56 +0800

    chore: use prev next component

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -5,6 +5,7 @@
 <template>
   <Progress />
   <div class="article" dark:text-gray-300 my-0 mx-auto>
+    <Toc />
     <ContentDoc class="content-doc">
       <template #empty>
         <h1>404</h1>
@@ -17,12 +18,29 @@
         <h2>😥</h2>
       </template>
     </ContentDoc>
+    <TopDownAndBack />
   </div>
-  <div mt-10>
+  <PrevNext :prev="prev" :next="next" />
+  <!-- <div mt-10> 返回
     <a font-mono opacity-50 hover:opacity-75 text-lg border-gray border-b dark:text-gray-300 hover:cursor-pointer @click="router.back()">cd ..</a>
-  </div>
+  </div> -->
 </template>
 
 <script lang="ts" setup>
-const router = useRouter()
+// const router = useRouter()
+const { path } = useRoute()
+console.log(path)
+
+const { data } = await useAsyncData(`content-${path}`, async () => {
+  // 根据当前 path 找寻本文的位置
+  const article = queryContent().where({ _path: path }).findOne()
+  // 查找文章，通过时间进行排序，利用 findSurround 查询本文环绕信息，哪个是位于本文的前面，哪个是位于本文的后面
+  const surround = queryContent().only(["_path", "title", "description", "date"]).sort({ date: 1 }).findSurround(path)
+  return {
+    article: await article,
+    surround: await surround
+  }
+})
+
+const [prev, next] = (data?.value!).surround
 </script>
