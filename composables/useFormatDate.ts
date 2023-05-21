9da89434b9d151cb6a4e9a898aaced22c69d1363
fan <291950304@qq.com>
2023-05-21 18:45:31 +0800

    fix: date format

--- composables/useFormatDate.ts
+++ composables/useFormatDate.ts
@@ -1,10 +1,18 @@
 const useFormatDate = (date: string) => {
-  const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }
-  const now = new Date(date).toLocaleDateString('zh-CN', options)
+  // const options: Intl.DateTimeFormatOptions = {
+  //   year: 'numeric',
+  //   month: 'long',
+  //   day: 'numeric',
+  // }
+  // const now = new Date(date).toLocaleDateString('zh-CN', options)
+  // return now
+
+  // toLocaleDateString 在手机中有显示 bug
+  const newDate = new Date(date)
+  const year = newDate.getFullYear()
+  const month = newDate.getMonth() + 1
+  const day = newDate.getDate()
+  const now = year + '年' + month + '月' + day + '日'
   return now
 }
 
