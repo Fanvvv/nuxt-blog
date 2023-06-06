c462147c440c90440c11ce3e93c5d5c5d453d6c7
fan <291950304@qq.com>
2023-06-06 16:12:24 +0800

    style: add animation

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-    <div fc flex-col relative ref="heightRef" dark:text-gray-100 >
-      <img w-40 h-40 border-rounded-40 transition-all duration-500 ease-in-out src="/images/me.png" alt="" />
-      <h1 font-400 text-2xl md:text-3xl mt-5>Fan</h1>
-      <span text-base md:text-xl>Front-end Developer</span>
-      <div fcc mt-3 text-base leading-5 dark:text-gray-100>
-        <a v-for="route in routes" :href="route.href" target="_blank" ml-5>
-          <span text-5 icon-btn :class="route.icon"></span>
-        </a>
-      </div>
-      <div w-130 mt-10>
-        你好！我是一名前端工程师，这个博客主要记录了一些学习成长笔记。
+    <div relative ref="heightRef" dark:text-gray-100 >
+      <div v-motion-pop-visible fc flex-col>
+        <img w-40 h-40 border-rounded-40 transition-all duration-500 ease-in-out src="/images/me.png" alt="" />
+        <h1 font-400 text-2xl md:text-3xl mt-5>Fan</h1>
+        <span text-base md:text-xl>Front-end Developer</span>
+        <div fcc mt-3 text-base leading-5 dark:text-gray-100>
+          <a v-for="route in routes" :href="route.href" target="_blank" ml-5>
+            <span text-5 icon-btn :class="route.icon"></span>
+          </a>
+        </div>
       </div>
       <!-- <div nextAnimate @click="handleScorll"><span icon-btn text-10 :class="randomIcon()"></span></div> -->
     </div>
+    <div w-130 mt-10 mx-auto my-0>
+        你好！我是一名前端工程师，这个博客主要记录了一些学习成长笔记。
+      </div>
     <div mt-7 text-center dark:text-gray-100>
       <h1>我的技术栈</h1>
       <div w-130 grid grid-cols-3 my-0 mx-auto>
